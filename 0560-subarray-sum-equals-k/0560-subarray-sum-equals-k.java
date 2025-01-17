7e9f55c07cd70ba8ac59715f56002d0f3f9c87b9
Abhishek Pal <106912617+Abhi0pal@users.noreply.github.com>
2025-01-17 12:04:16 +0530

    Time: 23 ms (91.73%), Space: 46.6 MB (50.7%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,18 +1,33 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        //Brute Force Approaches
+
+        // int count=0;
+        // for(int i=0;i<nums.length;i++){
+        //     int sum=0;
+        //     for(int j=i;j<nums.length;j++){
+        //         sum+=nums[j];
+        //         if(sum==k){
+        //             count++;
+                    
+        //         }
+        //     }
+        // }
+        // return count;
+
+        //Optimal PrefixSum Appraoch 
+        HashMap<Integer,Integer>mpp=new HashMap<>();
+        mpp.put(0,1);
+        int prefixSum=0;
         int count=0;
         for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum+=nums[j];
-                if(sum==k){
-                    count++;
-                    
-                }
+            prefixSum+=nums[i];
+            if(mpp.containsKey(prefixSum-k)){
+                count+=mpp.get(prefixSum-k);
             }
+            mpp.put(prefixSum,mpp.getOrDefault(prefixSum,0)+1);
         }
         return count;
-
         
     }
 }
